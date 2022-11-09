f454639f05df3580e86fa7218a3be408e1020c11
Roberto Guerra <15373732+RobertGuerra@users.noreply.github.com>
2022-11-08 16:49:43 -0800

    removed background image for main menu buttons

--- Connect4_AI/C4_main_AI.py
+++ Connect4_AI/C4_main_AI.py
@@ -192,14 +192,14 @@ def main_menu():
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-        MENU_TEXT = get_font(36).render("MAIN MENU", True, "#b68f40")
+        MENU_TEXT = get_font(36).render("MAIN MENU", True, WHITE)
         MENU_RECT = MENU_TEXT.get_rect(center=(300, 100))
 
-        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(300, 250),
+        PLAY_BUTTON = Button(None, pos=(300, 250),
                              text_input="PLAY", font=get_font(24), base_color=WHITE, hovering_color=ORANGE)
-        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(300, 400),
+        OPTIONS_BUTTON = Button(None, pos=(300, 350),
                                 text_input="OPTIONS", font=get_font(24), base_color="#d7fcd4", hovering_color=ORANGE)
-        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(300, 550),
+        QUIT_BUTTON = Button(None, pos=(300, 450),
                              text_input="QUIT", font=get_font(24), base_color="#d7fcd4", hovering_color=ORANGE)
 
         screen.blit(MENU_TEXT, MENU_RECT)
